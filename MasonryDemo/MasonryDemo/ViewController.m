32a5ca54400792bc051ff57853995af59c6bfeae
atbj505 <atbj505@hotmail.com>
2015-04-20 18:05:35 +0800

    2015.4.20

--- MasonryDemo/MasonryDemo/ViewController.m
+++ MasonryDemo/MasonryDemo/ViewController.m
@@ -11,12 +11,38 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UIButton *btn1;
+@property (strong, nonatomic) UIButton *btn2;
+@property (strong, nonatomic) UIButton *btn3;
+@property (strong, nonatomic) UILabel *lbl1;
+@property (strong, nonatomic) UILabel *lbl2;
+@property (strong, nonatomic) UILabel *lbl3;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.btn1 = [[UIButton alloc] init];
+    [self.btn1 setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:_btn1];
+    self.btn2 = [[UIButton alloc] init];
+    [self.btn2 setBackgroundColor:[UIColor yellowColor]];
+    [self.view addSubview:_btn2];
+    self.btn3 = [[UIButton alloc] init];
+    [self.btn3 setBackgroundColor:[UIColor greenColor]];
+    [self.view addSubview:_btn3];
+    self.lbl1 = [[UILabel alloc] init];
+    [self.lbl1 setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:_lbl1];
+    self.lbl2 = [[UILabel alloc] init];
+    [self.lbl2 setBackgroundColor:[UIColor yellowColor]];
+    [self.view addSubview:_lbl2];
+    self.lbl3 = [[UILabel alloc] init];
+    [self.lbl3 setBackgroundColor:[UIColor greenColor]];
+    [self.view addSubview:_lbl3];
     
     /*
      case1:view中心
@@ -34,6 +60,10 @@
      case4:scrollview添加view
     */
     //[self masScrollView];
+    /*
+     case5:cell中等间距控件
+     */
+    [self cellEqualWidth];
 }
 - (void)masCenter{
     UIView *view = [[UIView alloc]init];
@@ -126,6 +156,55 @@
     }
     [container mas_makeConstraints:^(MASConstraintMaker *make) {
         make.bottom.mas_equalTo(lastView.mas_bottom);
+    }];
+}
+
+static const int Y = 5;
+static const int X = 5;
+static const int GAP = 5;
+static const int LABELHEIGHT = 30;
+
+- (void)cellEqualWidth {
+    WS(weakSelf);
+    
+    [self.btn1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(weakSelf.view.mas_left).offset(X);
+        make.right.mas_equalTo(weakSelf.btn2.mas_left).offset(-GAP);
+        make.top.mas_equalTo(weakSelf.view.mas_top).offset(Y);
+        make.bottom.mas_equalTo(weakSelf.lbl1.mas_top).offset(-Y);
+        make.width.mas_equalTo(weakSelf.btn2.mas_width);
+    }];
+    [self.lbl1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.mas_equalTo(weakSelf.view.mas_bottom).offset(-GAP);
+        make.width.mas_equalTo(weakSelf.btn1.mas_width);
+        make.height.mas_equalTo(@(LABELHEIGHT));
+        make.centerX.mas_equalTo(weakSelf.btn1.mas_centerX);
+    }];
+    [self.btn2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(weakSelf.btn1.mas_right).offset(GAP);
+        make.right.mas_equalTo(weakSelf.btn3.mas_left).offset(-GAP);
+        make.top.mas_equalTo(weakSelf.view.mas_top).offset(Y);
+        make.bottom.mas_equalTo(weakSelf.lbl2.mas_top).offset(-Y);
+        make.width.mas_equalTo(weakSelf.btn3.mas_width);
+    }];
+    [self.lbl2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.mas_equalTo(weakSelf.view.mas_bottom).offset(-GAP);
+        make.width.mas_equalTo(weakSelf.btn2.mas_width);
+        make.height.mas_equalTo(@(LABELHEIGHT));
+        make.centerX.mas_equalTo(weakSelf.btn2.mas_centerX);
+    }];
+    [self.btn3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(weakSelf.btn2.mas_right).offset(GAP);
+        make.right.mas_equalTo(weakSelf.view.mas_right).offset(-GAP);
+        make.top.mas_equalTo(weakSelf.view.mas_top).offset(Y);
+        make.bottom.mas_equalTo(weakSelf.lbl3.mas_top).offset(-Y);
+        make.width.mas_equalTo(weakSelf.btn1.mas_width);
+    }];
+    [self.lbl3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.mas_equalTo(weakSelf.view.mas_bottom).offset(-GAP);
+        make.width.mas_equalTo(weakSelf.btn3.mas_width);
+        make.height.mas_equalTo(@(LABELHEIGHT));
+        make.centerX.mas_equalTo(weakSelf.btn3.mas_centerX);
     }];
 }
 @end
